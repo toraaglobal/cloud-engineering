01708d8216a323b189d72396f0ee0cadd8c1c151
Tajudeen Abdulazeez <tabdulazeez99@gmail.com>
2022-08-18 00:32:18 -0700

    pollynotes capstone

---
+++ dev-on-aws/polly-apps/capstone/web/src/Config.js
@@ -0,0 +1,12 @@
+//Variables to replace
+const poolId = 'us-east-1_p4huhZR9l'
+const clientId = '2bo1fke1u94s4nqmdgvtbf21mf'
+const api = 'https://lcbfxe7jtc.execute-api.us-west-2.amazonaws.com/Prod'
+
+const Config = {
+    UserPoolId: poolId,
+    AppClientId: clientId,
+    ApiURL: api + '/notes'
+}
+
+export default Config
